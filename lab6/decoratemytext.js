c3abdd0bb2c9e37bdc789ec96b984bfcac34960a
gakomeng <gakomeng@nalosolutions.com>
2020-01-19 21:46:10 -0600

    lab6

--- lab6/decoratemytext.js
+++ lab6/decoratemytext.js
@@ -1,19 +1,25 @@
 window.onload = function() {
-  //"use strict";
+  "use strict"; //use strict mode
 
-  let textArea = this.document.getElementById("textArea");
-  bigger = () => {
+  //get relevant HTML elements using their ids // onubstrusive
+  const textArea = this.document.getElementById("textArea");
+  const decorateBtn = this.document.getElementById("decorateBtn");
+  const chkbox = this.document.getElementById("bling");
+
+  //* Method definitions *//
+
+  const startGrowing = () => {
     this.setInterval(growBigger, 500);
   };
-  growBigger = () => {
+  const growBigger = () => {
     //textArea.style.fontSize = "2rem";
     let oldfontsize = textArea.style.fontSize;
     if (!oldfontsize) oldfontsize = "12pt";
     let newfontsize = this.parseInt(oldfontsize) + 2;
     textArea.style.fontSize = newfontsize + "pt";
   };
-  ischecked = id => {
-    let chkbox = this.document.getElementById(id);
+
+  const decorate = () => {
     if (chkbox.checked) {
       textArea.style.fontWeight = "bold";
       textArea.style.color = "green";
@@ -27,4 +33,8 @@ window.onload = function() {
       this.window.document.body.style.background = "white";
     }
   };
+
+  //Use by reference
+  decorateBtn.onclick = startGrowing;
+  chkbox.onchange = decorate;
 };
