c68c3532d76d0540c876bd24c7e5ae5e89e27050
gakomeng <gakomeng@nalosolutions.com>
2020-01-28 23:32:02 -0600

    lab11

--- lab11/maze.js
+++ lab11/maze.js
@@ -1,24 +1,42 @@
 $(function() {
-  $('#start').click(function() {
-    $('.boundary').removeClass('youlose');
-    $('#status').text('Start moving');
+  const maze = (function() {
+    const reset = (text) => {
+      $('.boundary').removeClass('youlose');
+      $('#status').text(text);
+      $('#start, #end').css('display', ''); // show start and end divs
+    };
 
-
-    $('.boundary').on('mouseenter', function() {
-      $('.boundary').addClass('youlose');
-      $('#status').text('You lose:(');
-    });
-
-    $('#maze').on('mouseleave', function() {
+    const lose = () => {
       $('.boundary').addClass('youlose');
       $('#status').text('You lose:(');
       $('#end').off('mouseover');
-    });
+      $('.boundary').off('mouseenter');
+      $('#maze').off('mouseleave');
 
-    $('#end').on('mouseover', function() {
+      $('#start, #end').css('display', 'none');
+      // reset automatically after 3 seconds
+      setTimeout(reset, 3000, 'Click the "S" to begin.');
+    };
+    const win = () => {
       $('#status').text('You win!');
       $('.boundary').off('mouseenter');
       $('#maze').off('mouseleave');
-    });
-  });
+      $('#end').off('mouseover');
+      $('#start, #end').css('display', 'none'); // hide start and end divs
+      // reset automatically after 3 seconds
+      setTimeout(reset, 3000, 'Click the "S" to begin.');
+    };
+    const start = ()=>{
+      // reset the maze
+      reset('Start moving');
+      $('.boundary').on('mouseenter', lose);
+      $('#maze').on('mouseleave', lose);
+      $('#end').on('mouseover', win);
+    };
+    return {start: start};
+  })();
+  /**
+   * Using the Revealing model, only the start method is visible;
+   */
+  $('#start').click(maze.start);
 });
