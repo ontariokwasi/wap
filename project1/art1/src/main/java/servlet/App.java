827f333c8adc62bbde300b868bbdbac8deb7b30e
610530 <gakomeng@mum.edu>
2020-01-30 15:43:19 -0600

    classwork

---
+++ project1/art1/src/main/java/servlet/App.java
@@ -0,0 +1,20 @@
+package servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends HttpServlet
+{
+    /**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
